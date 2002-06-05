5d5925f82c8a4cbc2b2f188233fcb7e6adcdbaa0
(no author) <(no author)@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2002-06-05 18:10:19 +0000

    This commit was manufactured by cvs2svn to create tag 'amsn-0_52'.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@31 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ tags/amsn-0_52/amsn/smileys.tcl
@@ -0,0 +1,48 @@
+#
+# $Id$
+#
+set emotions {{":-)" smile} {":)" smile} {":-D" smiled} {":D" smiled}
+	{":->" smiled} {":>" smiled} {":-O" smileo} {":O" smileo} {":-P" smilep}
+	{":P" smilep} {";-)" wink} {";)" wink} {":-(" sad} {":(" sad}
+	{":-<" sad} {":<" sad} {":-S" crooked} {":S" crooked} {":-|" disgust}
+	{":|" disgust} {"(Y)" thumbu} {"(N)" thumbd} {"(L)" love} {"(U)" unlove}
+	{"(K)" lips} {"(G)" gift} {"(F)" rose} {"(X)" emgirl} {"(Z)" emboy}
+	{"(P)" photo} {"(B)" beer} {"(D)" coctail} {"(T)" emphone} {"(@)" emcat}
+	{"(C)" emcup} {"(I)" embulb} {"(H)" emhottie} {"(S)" emsleep}
+	{"(*)" emstar} {"(8)" emnote} {"(E)" email} {"(M)" messenger}
+	{":-[" vampire} {":[" vampire} {"(\})" girlhug} {"(\{)" boyhug}
+	{"(A)" angel} {"(6)" devil} {"(^)" cake} {"(O)" clk} {":-@" angry}
+	{":@" angry} {"(&)" dog} {"(W)" rosew} {":`(" smilec} {":$" smilemb} 
+	{":-$" smilemb} {"(#)" sun} {"(R)" rainbow}}
+
+set emotion_files {smile smiled smileo smilep wink sad crooked disgust thumbu
+	thumbd love unlove lips gift rose emgirl emboy photo beer coctail
+	emphone emcat emcup embulb emhottie emsleep emstar emnote email
+	messenger vampire girlhug boyhug angel devil cake clk angry dog rosew
+	smilec smilemb smilemb sun rainbow}
+
+
+proc smile_subst {tw} {
+  global emotions
+
+#      tw mark set new_text_start end
+#      tw insert $section.last "$user_name$state_desc\n" $user_login
+
+   foreach emotion $emotions {
+	   
+      set symbol [lindex $emotion 0]
+      set file [lindex $emotion 1]
+      set chars [string length $symbol]
+      
+      while {[set pos [$tw search -exact -nocase \
+	                              $symbol 0.0 end]] != ""} {
+         set posyx [split $pos "."]
+         set endpos "[lindex $posyx 0].[expr [lindex $posyx 1] + $chars]"
+         $tw delete $pos $endpos
+
+         $tw image create $pos -image $file -pady 1 -padx 1
+
+      }
+   }
+
+}
