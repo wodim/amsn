66f2cbfb9766805e1ccb754a3a93604ead3c6724
thaven <thaven@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-01-23 20:47:07 +0000

    Apply fix from -r9362: add protocol debug output for MSNP2P data received on direct connections. Show only packets with an offset of 0 to avoid flooding the protocol window when receiving a file (only first chunk will be shown though..) Also made the fileevent that writes on the DC socket wait 5 ms before getting called. When doing direct transfer on a local network, aMSN would COMPLETELY freeze, using 100% of CPU, impossible to do anything, it doesn't even refresh the window.. I calculated that 5ms should be enough without dropping much of performance. I added the 5ms timer and aMSN became fully functional, as if no transfer was being done. The transfer speed went from 540KB/s to 520KB/s so it's good! :)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@9498 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- branches/0_97/amsn/protocol.tcl
+++ branches/0_97/amsn/protocol.tcl
@@ -6695,7 +6695,11 @@ namespace eval ::MSN6FT {
 	
 					set p2pmessage [P2PMessage create %AUTO%]
 					$p2pmessage createFromMessage $message
-				
+
+					if { [$p2pmessage cget -offset] == 0 } {
+						degt_protocol "<--DC-MSNP2P ([getObjOption $sid chatid]) $data" "sbrecv"
+					}
+
 					::MSNP2P::ReadData $p2pmessage [getObjOption $sid chatid]
 					catch { $p2pmessage destroy }
 					catch { $message destroy }
@@ -6771,7 +6775,7 @@ namespace eval ::MSN6FT {
 					fileevent $sock writable ""
 					catch { close $sock }
 				} else {
-					fileevent $sock writable "::MSN6FT::WriteToSock $sock"
+					after 5 [list fileevent $sock writable "::MSN6FT::WriteToSock $sock"]
 				}
 			}
 
