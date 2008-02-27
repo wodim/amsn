3980a69dab93d479cca453550cd72ef05bf80d6a
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-02-27 05:26:30 +0000

    Conditional load for webcamsn, in case we need to ship a version without wmv3 support (like for fedora) because of license issues, and have a non-official package adding a binary with wmv3 support
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@9606 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- branches/video_conf/utils/webcamsn/pkgIndex.tcl
+++ branches/video_conf/utils/webcamsn/pkgIndex.tcl
@@ -2,4 +2,8 @@
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-package ifneeded webcamsn 0.1 "package require Tk; [list load [file join $dir webcamsn[info shared]] webcamsn];package provide webcamsn 0.1"
+if { [file exists [file join $dir webcamsn-av[info shared]]] } {
+    package ifneeded webcamsn 0.2 "package require Tk; [list load [file join $dir webcamsn-av[info shared]] webcamsn];package provide webcamsn 0.2"
+} else {
+    package ifneeded webcamsn 0.2 "package require Tk; [list load [file join $dir webcamsn[info shared]] webcamsn];package provide webcamsn 0.2"
+}
