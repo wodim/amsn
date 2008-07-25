8a31cb820b0bb2e0bc928d482a2c73e11ce120c6
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-07-25 13:43:32 +0000

    Merge the following from trunk to 0_97 branch: 10309 - kakaroto - 1px border notify window 10318 - kakaroto - flickering issue
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@10324 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- branches/0_97/amsn/chatwindow.tcl
+++ branches/0_97/amsn/chatwindow.tcl
@@ -3731,7 +3731,7 @@ namespace eval ::ChatWindow {
 		::ChatWindow::UpdateContainerTitle $container
 
 		# Should fix the issue with tab switching and the input not focusing.
-		update idletasks
+		#update idletasks
 
 #		bind $win <Map> [list focus [::ChatWindow::GetInputText $win] ]
 
