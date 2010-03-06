a4734dd4271bf8b5d105fbaaeab84c3457875541
billiob <billiob@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2010-03-06 22:00:46 +0000

    readd 0_98 branch
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@11972 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ branches/0_98/utils/linux/capture/libng/Rules.mk
@@ -0,0 +1,26 @@
+OBJS-libng := \
+	$(capture_dir)/libng/grab-ng.o \
+	$(capture_dir)/libng/devices.o \
+	$(capture_dir)/libng/writefile.o \
+	$(capture_dir)/libng/parse-mpeg.o \
+	$(capture_dir)/libng/parse-dvb.o \
+	$(capture_dir)/libng/color_common.o \
+	$(capture_dir)/libng/color_packed.o \
+	$(capture_dir)/libng/color_lut.o \
+	$(capture_dir)/libng/color_yuv2rgb.o \
+	$(capture_dir)/libng/convert.o \
+	$(capture_dir)/libng/misc.o \
+	$(TARGETS-plugins)
+
+TARGET-libng := $(capture_dir)/libng/libng.a
+
+$(TARGET-libng): $(OBJS-libng)
+	@$(echo_ar_lib)
+	@$(ar_lib)
+
+all:: $(TARGET-libng)
+
+clean:: clean-libng
+
+clean-libng:
+	rm -f $(TARGET-libng) $(OBJS-libng)
