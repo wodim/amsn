be60b976fea8d35efa921880a86c2ff7e7c8287d
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2010-11-06 20:41:21 +0000

    Kill inactive SBs on my branch
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@12203 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- branches/p2p/protocol.tcl
+++ branches/p2p/protocol.tcl
@@ -2436,6 +2436,14 @@ namespace eval ::MSN {
 			::MSN::CloseSB $sbn
 			degt_protocol "->$sbn FAILED: $cmd" error
 		}
+		if { $sbn != "ns" } {
+			if { [$sbn cget -killme] != "" } {
+				after cancel [$sbn cget -killme]
+			}
+			if { $cmd != "OUT\r\n" } {
+				$sbn configure -killme [after 60000 "::MSN::CloseInactiveSB $sbn"]
+			}
+		}
 	}
 
 
@@ -2580,6 +2588,11 @@ namespace eval ::MSN {
 
 		set oldstat [$sb cget -stat]
 		$sb configure -stat "d"
+
+		if { [$sb cget -killme] != "" } {
+			after cancel [$sb cget -killme]
+			$sb configure -killme ""
+		}
 
 		if { [string match -nocase "*ns*" $sb] } {
 			status_log "clearing sb $sb. oldstat=$oldstat"
@@ -3016,6 +3029,16 @@ namespace eval ::MSN {
 		}
 
 		return $lowuser
+	}
+
+	########################################################################
+	proc CloseInactiveSB { sb } {
+
+		if { [info commands $sb] == "" } { return }
+		$sb configure -killme ""
+		WriteSBRaw $sb "OUT\r\n"
+		CloseSB $sb
+
 	}
 
 
@@ -5114,6 +5137,7 @@ namespace eval ::MSNOIM {
 	option -auth_param ""
 	option -last_activity 0
 	option -lastspoke 0
+	option -killme ""
 
 	constructor {args} {
 		install connection using Connection %AUTO% -name $self
@@ -5595,6 +5619,10 @@ namespace eval ::MSNOIM {
 			}
 		}
 		$message destroy
+		if { $options(-killme) != "" } {
+			after cancel $options(-killme)
+		}
+		$self configure -killme [after 60000 [list ::MSN::CloseInactiveSB $self]]
 	}
 
 	method search { option index } {
