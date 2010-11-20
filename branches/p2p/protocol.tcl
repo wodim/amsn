76760e47ed4f58891cebf6253233acf6ab00b4c6
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2010-11-20 00:37:28 +0000

    one more sb killme fix
    git-svn-id: svn://svn.code.sf.net/p/amsn/code@12235 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- branches/p2p/protocol.tcl
+++ branches/p2p/protocol.tcl
@@ -2439,6 +2439,7 @@ namespace eval ::MSN {
 			::MSN::CloseSB $sbn
 			degt_protocol "->$sbn FAILED: $cmd" error
 		}
+		if { [info commands $sb] == "" } { return }
 		if { $sbn != "ns" } {
 			if { [$sbn cget -killme] != "" } {
 				after cancel [$sbn cget -killme]
